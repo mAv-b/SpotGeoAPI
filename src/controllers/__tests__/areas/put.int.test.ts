ce05bdbe6c72f3f6700e4d8ac6fd10fab7144a19
Mark <marcosrafaelp.camargo@gmail.com>
2023-04-12 15:39:56 -0300

    refactor: workflow

--- src/controllers/__tests__/areas/put.int.test.ts
+++ src/controllers/__tests__/areas/put.int.test.ts
@@ -1,17 +1,17 @@
 describe('Area Tests: PUT', () => {
 
   // TEST: edit successfully a area by id
-  test('should return a OK status for edit a area by id', async () => {});
+  test('should return a OK status for edit a area by id', async () => {return});
 
   // TEST: invalid payload
-  test('should return a bad request for invalid payload', async () => {});
+  test('should return a bad request for invalid payload', async () => {return});
 
   // TEST: not found area for edit
-  test('should return a not found area', async () => {});
+  test('should return a not found area', async () => {return});
 
   // TEST: invalid id for area
-  test('should return a bad request for invalid id', async () => {});
+  test('should return a bad request for invalid id', async () => {return});
 
   // TEST: unauthorizate request
-  test('should return a forbbiden status', async () => {});
+  test('should return a forbbiden status', async () => {return});
 });
