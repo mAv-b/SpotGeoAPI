407693342fbff01466fe8e9b810e39dadf3d0b3d
Mark <marcosrafaelp.camargo@gmail.com>
2023-04-12 15:00:28 -0300

    refactor: db/init

--- src/database/init.ts
+++ src/database/init.ts
@@ -1,27 +1,140 @@
 import * as dotenv from 'dotenv';
 import * as bcrypt from 'bcrypt';
-import sequelize from ".";
+import { Client } from 'pg';
+import { Sequelize } from 'sequelize';
 import { userAttributtes } from "./models/User";
 import { placeAttributtes } from './models/Place';
 import { areaAttributes } from './models/Area';
 
 dotenv.config();
 
-const User = sequelize.define("users", userAttributtes);
-const Place = sequelize.define("places", placeAttributtes);
-const Area = sequelize.define("area", areaAttributes);
+const createDatabase = async () => {
+  const client = new Client({
+    host: process.env.HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USERNAME,
+    password: process.env.PASSWORD
+  })
 
-sequelize.sync().then(()=>{
-  const salt = bcrypt.genSaltSync(10);
+  try {
+    await client.connect();
 
-  User.create({
-    email: "admin@email.com",
-    name: "admin",
-    password: bcrypt.hashSync("pass", salt)
+    const databaseQuery = `CREATE DATABASE ${process.env.DATABASE}`;
+
+    await client.query(databaseQuery);
+
+  } catch(err) {
+    console.error("opa");
+  } finally {
+    await client.end();
+  }
+}
+
+const migrateData = async () => {
+
+  const sequelize = new Sequelize(
+    process.env.DATABASE!,
+    process.env.DB_USERNAME!,
+    process.env.PASSWORD!,
+    {
+      host: process.env.HOST!,
+      port: Number(process.env.DB_PORT!),
+      dialect: 'postgres',
+      logging: false
+    })
+
+  const addPostGis = `CREATE EXTENSION postgis`;
+  await sequelize.query(addPostGis);
+
+  const User = sequelize.define("users", userAttributtes);
+  const Place = sequelize.define("places", placeAttributtes);
+  const Area = sequelize.define("area", areaAttributes);
+
+  sequelize.sync().then(() => {
+    const salt = bcrypt.genSaltSync(10);
+
+    User.create({
+      email: "admin@email.com",
+      name: "admin",
+      password: bcrypt.hashSync("pass", salt)
+    });
+
+  }).catch(err => {
+    console.error("Error in initializate database:", err);
+  });
+}
+
+const cleanDatabase = async () => {
+  const client = new Client({
+    database: process.env.DATABASE,
+    host: process.env.HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USERNAME,
+    password: process.env.PASSWORD
+  })
+
+  try {
+    await client.connect();
+
+    const sqlDeleteUsersTable = `DROP TABLE users`;
+    const sqlDeletePlacesTable = `DROP TABLE places`;
+    const sqlDeleteAreasTable = `DROP TABLE areas`;
+
+    await client.query(sqlDeleteUsersTable);
+
+    await client.query(sqlDeletePlacesTable);
+
+    await client.query(sqlDeleteAreasTable);
+
+  } catch(err) {
+    console.error(err);
+  } finally {
+    client.end();
+  }
+}
+
+// check env variables
+if (!process.env.DATABASE ||
+  !process.env.DB_USERNAME ||
+  !process.env.PASSWORD ||
+  !process.env.HOST ||
+  !Number(process.env.DB_PORT)) {
+
+  throw new Error(
+    "ERROR IN CONNECT TO DATABASE: please check the env file values."
+  );
+
+} else if (process.env.DATABASE.toLowerCase() !== process.env.DATABASE) {
+  throw new Error(
+    "Please use only lower case in database name"
+  )
+}
+
+// flow to (create a database) to (migrate data)
+const command = process.argv.splice(2)[0];
+
+if (command === '--DB') {
+  createDatabase().then(() => {
+    console.log('DATABASE CREATED');
+  }).catch(_ => {
+    console.log('FAIL IN CREATE DATABASE');
   });
 
-  console.log("Initializated.");
-  
-}).catch(err =>{
-  console.error("Error in initializate database:", err);
-});
+} else if (command === '--clean') {
+
+  cleanDatabase().then(() => {
+    console.log('DATABASE CLEANED');
+  }).catch(_ => {
+    console.log('FAIL TO CLEAN DATABASE');
+  })
+
+} else {
+
+  migrateData().then(() => {
+    console.log('MIGRATION COMPLETE');
+  }).catch(_ => {
+    console.log(_);
+    console.log('FAIL IN MIGRATE')
+  });
+
+}
